e446bdb6253d6b6787280f9997e0cfd1aac872e9
namsoo5 <are34@naver.com>
2020-05-18 23:25:15 +0900

    UPDATE: 자동로그인 로직구현

--- SOPT_iOS_Login/SOPT_iOS_Login/Screens/Main/MainViewController.swift
+++ SOPT_iOS_Login/SOPT_iOS_Login/Screens/Main/MainViewController.swift
@@ -12,20 +12,40 @@ class MainViewController: UIViewController {
     @IBOutlet weak var signUpLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var autoLoginCheckBox: UIView!
+    @IBOutlet weak var autoLoginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationColor()
         signUpLabel.underLine()
+        setupCheckBox()
     }
     
     func setNavigationColor() {
-        // 네비게이션 영역 그라데이션색
-        // navigationController?.navigationBar.backgroundColor = .red
         // 툴바 버튼색
         navigationController?.navigationBar.tintColor = .blue
         // 네비게이션 영역색
         navigationController?.navigationBar.barTintColor = .white
+    }
+    
+    func setupCheckBox() {
+        autoLoginCheckBox.layer.borderColor = UIColor.lightGray.cgColor
+        autoLoginCheckBox.layer.borderWidth = 1
+    }
+    
+    func autoCheckAnimation() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.5, options: [.allowUserInteraction, .curveLinear], animations: {
+            self.autoLoginCheckBox.backgroundColor = #colorLiteral(red: 0.5901310444, green: 0.4440979362, blue: 1, alpha: 1)
+            self.autoLoginCheckBox.transform = CGAffineTransform(scaleX: 1.15, y: 1.15)
+        })
+    }
+    
+    func autoUnCheckAnimation() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.5, options: [.allowUserInteraction, .curveLinear], animations: {
+            self.autoLoginCheckBox.backgroundColor = .white
+            self.autoLoginCheckBox.transform = .identity
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,6 +53,12 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func loginClick(_ sender: Any) {
+        let isAutoLogin = autoLoginButton.isSelected
+        
+        if isAutoLogin {
+            UserDefaults.standard.set(true, forKey: UserDefaultName.isAutoLogin.rawValue)
+        }
+        
         guard let nextVC = storyboard?.instantiateViewController(withIdentifier: "TabBarC") else {
             return
         }
@@ -45,14 +71,9 @@ class MainViewController: UIViewController {
         }
         navigationController?.pushViewController(nextVC, animated: true)
     }
-}
-
-extension UIView {
-    open override func awakeFromNib() {
-        super.awakeFromNib()
-        // UIView의 tag가 1인경우 둥글게
-        if self.tag == 1{
-            self.layer.cornerRadius = self.frame.height/2
-        }
+    
+    @IBAction func autoLoginClick(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        sender.isSelected ? autoCheckAnimation() : autoUnCheckAnimation()
     }
 }
---
+++ SOPT_iOS_Login/SOPT_iOS_Login/Support/UserDefaultName.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultName.swift
+//  SOPT_iOS_Login
+//
+//  Created by 남수김 on 2020/05/18.
+//  Copyright © 2020 남수김. All rights reserved.
+//
+
+import Foundation
+
+enum UserDefaultName: String {
+    case isAutoLogin = "isAutoLogin"
+}
